1996069393f74c4514a6bbe33ffb09e08706c78b
Lucas Riffel <lucas26.riffel@gmail.com>
2024-02-04 00:52:36 -0300

    update(funcionando)

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let divMoedas = document.querySelector("#moedas")
+document.querySelector(".botao-efetuar").addEventListener("click", function() {
+    const moedaOrigem = document.querySelector(".moedaConversao").value.toUpperCase();
+    const moedaDestino = document.querySelector(".moedaConvertida").value.toUpperCase();
+    const valorOriginal = parseFloat(document.querySelector(".moeda1").value);
 
+    fetch("moedas.json")
+        .then(response => response.json())
+        .then(data => {
+            const taxaConversao = data.valor[moedaDestino] / data.valor[moedaOrigem];
+            const valorConvertido = valorOriginal * taxaConversao;
 
-fetch("moedas.json").then((Response) => {
-    Response.json().then((moedas) => {
-        divMoedas.innerHTML += moedas.conversao.BRL
-    })
-})
+            document.querySelector(".moeda2").value = valorConvertido.toFixed(2);
+        })
+        .catch(error => console.error("Erro na requisição:", error));
+});
