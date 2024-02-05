bcc41aca568397e76d469039c76dc950907eab82
Lucas Riffel <lucas26.riffel@gmail.com>
2024-02-05 00:52:16 -0300

    update

--- script.js
+++ script.js
@@ -34,19 +34,41 @@ const taxasDeCambio = ({
     "ZAR": 18.8978426152
 });
 
+function limparMoeda() { 
+    document.querySelector(".moedaConversao").value = "";
+    document.querySelector(".moedaConvertida").value = "";
+    document.querySelector(".moeda1").value = "";
+}
+
+document.getElementById("limparMoedas").addEventListener("click", limparMoeda);
+
+function trocarMoeda() { 
+    const valorMoedaConversao = document.querySelector(".moedaConversao").value;
+    const valorMoedaConvertida = document.querySelector(".moedaConvertida").value;
+   
+
+    document.querySelector(".moedaConversao").value = valorMoedaConvertida;
+    document.querySelector(".moedaConvertida").value = valorMoedaConversao;
+    document.querySelector(".moeda1").value = "";
+}
 
 document.querySelector(".botao-efetuar").addEventListener("click", function () {
     const moedaOrigem = document.querySelector(".moedaConversao").value.toUpperCase();
     const moedaDestino = document.querySelector(".moedaConvertida").value.toUpperCase();
     const valorOriginal = parseFloat(document.querySelector(".moeda1").value);
+    
 
     if (moedaOrigem in taxasDeCambio && moedaDestino in taxasDeCambio) {
         const taxaConversao = taxasDeCambio[moedaDestino] / taxasDeCambio[moedaOrigem];
         const valorConvertido = valorOriginal * taxaConversao;
 
-
         document.querySelector(".moeda2").value = valorConvertido.toFixed(2);
     } else {
         alert('Moedas inválidas selecionadas');
+        limparMoeda;
     }
 });
+
+
+
+document.getElementById("trocarMoedas").addEventListener("click", trocarMoeda);
